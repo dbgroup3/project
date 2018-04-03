9954d2c11f33b40fe82bd740bf0edab6c7ab3eee
ericl1ericl <ericl1ericl@aol.com>
2018-04-03 17:16:08 +0000

    update sql, add shell script

---
+++ make_song_db.sh
@@ -0,0 +1,9 @@
+sqlplus -S guest/guest @stations.sql
+
+sqlplus -S guest/guest @songs.sql
+
+sqlplus -S guest/guest @users.sql
+
+sqlplus -S guest/guest @song_queue.sql
+
+sqlplus -S guest/guest @user_permissions.sql
